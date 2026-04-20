72b30dd572fd71a2219de566b0ad38c5d4c12c30
akansha.mandal@projecttech4dev.org <akansha.mandal@projecttech4dev.org>
2026-04-20 12:20:45 +0000

    intermediate

--- models/intermediate/order_stores_join_clean.sql
+++ models/intermediate/order_stores_join_clean.sql
@@ -3,7 +3,7 @@
 
 {{ config(materialized='table', schema='intermediate') }}
 WITH cte1 as (
-SELECT "customer", "store_id", "tax_rate", "id", "subtotal", "tax_paid", "ordered_at", "order_total", "opened_at", "name", "id_2"
+SELECT "customer", "store_id", "id", "subtotal", "ordered_at", "order_total", "opened_at", "name"
 FROM {{ ref('order_stores_join') }}
 )
 -- Final SELECT statement combining the outputs of all CTEs
