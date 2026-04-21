94817b5e4fa12d4772ed86cfc61724e4644f740f
Ritabrata Roy <48592746+royred98@users.noreply.github.com>
2026-04-21 09:58:06 +0530

    Delete models directory
    deleted due to issues on canvas

--- models/intermediate/recovery_rate.sql
+++
@@ -1,9 +0,0 @@
-{{ config(
-  materialized='table'
-) }}
-
-SELECT "Country_Region",
-       SUM(CAST("Recovered" AS INTEGER)) / NULLIF(SUM(CAST("Confirmed" AS INTEGER)), 0) * 100 AS Recovery_Rate_Percent
-FROM  {{ source('source_demo', 'covid19') }}
-GROUP BY "Country_Region"
-ORDER BY Recovery_Rate_Percent DESC
